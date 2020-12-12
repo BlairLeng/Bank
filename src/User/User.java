a7a1410281942b5a10d282e213aca0c8d96b9d77
Ziang Leng <zaleng@bu.edu>
2020-12-12 22:13:58 +0800

    some changes

--- src/User/User.java
+++ src/User/User.java
@@ -6,12 +6,25 @@ public class User {
 	private String username;
 	private String password;
 	private String type;
-	private ArrayList<String> AccountIDs;
+//	private ArrayList<String> AccountIDs;
 	
-	public User(String username, String password, String type, ArrayList<String> AccountIDs) {
+//	public User(String username, String password, String type, ArrayList<String> AccountIDs) {
+	public User(String username, String password, String type) {
 		this.username = username;
 		this.password = password;
 		this.type = type;
-		this.AccountIDs = AccountIDs;
+//		this.AccountIDs = AccountIDs;
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public String getType() {
+		return this.type;
 	}
 }
