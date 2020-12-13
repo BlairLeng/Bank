452086190ef268eb3db90d3c0b1e702fdedeeb46
Ziang Leng <zaleng@bu.edu>
2020-12-13 18:03:52 +0800

    add more tables

--- src/AccountSystem/AccountSystemFunctions.java
+++ src/AccountSystem/AccountSystemFunctions.java
@@ -1,5 +1,14 @@
 package AccountSystem;
 
 public interface AccountSystemFunctions {
-
+	
+	// queryAccount
+	
+	// make loan
+	
+	// make transaction
+	
+	// deposit
+	
+	// withdraw
 }
--- src/Database/DatabaseTables.java
+++ src/Database/DatabaseTables.java
@@ -19,5 +19,32 @@ public class DatabaseTables {
 			"  PRIMARY KEY (AccountID),\r\n" + 
 			"  CONSTRAINT Username FOREIGN KEY (Username) REFERENCES user(Username)\r\n" + 
 			");";
+	
+	public static final String loanTable = "CREATE TABLE IF NOT EXISTS Loan  (\r\n" + 
+			"  LoanID varchar(100) NOT NULL,\r\n" + 
+			"  LoanName varchar(100),\r\n" +
+			"  LoanReason varchar(100),\r\n" +
+			"  AccountID varchar(100) NOT NULL,\r\n" + 
+			"  BeginDatatime datetime(0) NOT NULL,\r\n" + 
+			"  EndDatatime datetime(0) NOT NULL,\r\n" + 
+			"  MoneyLoaned DOUBLE PRECISION(20, 4) NOT NULL,\r\n" + 
+			"  MoneyReturned DOUBLE PRECISION(20, 4) NOT NULL,\r\n" + 
+			"  InterestRate DOUBLE PRECISION(20, 4) NOT NULL,\r\n" + 
+			"  Completed bit(255) NOT NULL,\r\n" + 
+			"  CONSTRAINT AccountID FOREIGN KEY (AccountID) REFERENCES Account.(AccountID)\r\n" + 
+			"  PRIMARY KEY (LoanID)\r\n" + 
+			");";
+	
+	public static final String transactionTable = "CREATE TABLE IF NOT EXISTS Trans  (\r\n" + 
+			"  TransID varchar(100) NOT NULL,\r\n" + 
+			"  TransName varchar(100),\r\n" +
+			"  SenderID varchar(100) NOT NULL,\r\n" + 
+			"  ReceiverID varchar(100) NOT NULL,\r\n" + 
+			"  Money DOUBLE PRECISION(20, 4) NOT NULL,\r\n" + 
+			"  Datetime datetime(0) NOT NULL,\r\n" + 
+			"  CONSTRAINT SenderID FOREIGN KEY (AccountID) REFERENCES Account.(AccountID)\r\n" + 
+			"  CONSTRAINT ReceiverID FOREIGN KEY (AccountID) REFERENCES Account.(AccountID)\r\n" + 
+			"  PRIMARY KEY (TransID)\r\n" + 
+			");";
 }
 	
