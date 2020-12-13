4a698f09ed182dd30a6ab7bee48a946df7b92391
Ziang Leng <zaleng@bu.edu>
2020-12-13 23:41:56 +0800

    account sys

--- src/AccountSystem/AccountSystemSQL.java
+++ src/AccountSystem/AccountSystemSQL.java
@@ -1,5 +1,117 @@
 package AccountSystem;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import Account.Account;
+import Common.Common;
+import TransactionSystem.Transaction;
+
 public class AccountSystemSQL {
 
+	public static ResultSet QueryAccountInformation(String AccountID, Connection con) throws SQLException {
+		Statement stmt = con.createStatement();
+		String sql = "SELECT * FROM account WHERE AccountID = "
+					+ "'"
+					+ AccountID
+					+ "'";
+		ResultSet rs = stmt.executeQuery(sql);
+		return rs;
+	}
+	
+	public static String MakeTransaction(Transaction t, Connection con) throws SQLException {
+		Statement stmt = con.createStatement();
+		String sql = "INSERT INTO `trans` "
+				+ "(`TransID`,`TransName`,`SenderID`,`ReceiverID`,`Money`,`Datetime`) "
+				+ "VALUES (" 
+				+ '"' 
+				+ t.getTransTime() 
+				+ '"' 
+				+ ", " 
+				+ '"'
+				+ t.gettransname()  
+				+ '"'
+				+ ", " 
+				+ '"' 
+				+ t.getsenderUUID() 
+				+ '"' 
+				+ ", " 
+				+ '"' 
+				+ t.getreceiverUUID() 
+				+ '"' 
+				+ ", "  
+				+ t.getmoney() 
+				+ ", " 
+				+ '"' 
+				+ t.gettransUUID()
+				+ '"' 
+				+ ")";
+		ResultSet rs = stmt.executeQuery(sql);
+		return Common.Success;
+	}
+	
+	public static String Deposit(Transaction t, Connection con) throws SQLException {
+		Statement stmt = con.createStatement();
+		String sql = "INSERT INTO `trans` "
+				+ "(`TransID`,`TransName`,`SenderID`,`ReceiverID`,`Money`,`Datetime`) "
+				+ "VALUES (" 
+				+ '"' 
+				+ t.getTransTime() 
+				+ '"' 
+				+ ", " 
+				+ '"'
+				+ t.gettransname()  
+				+ '"'
+				+ ", " 
+				+ '"' 
+				+ t.getsenderUUID() 
+				+ '"' 
+				+ ", " 
+				+ '"' 
+				+ t.getreceiverUUID() 
+				+ '"' 
+				+ ", "  
+				+ t.getmoney() 
+				+ ", " 
+				+ '"' 
+				+ t.gettransUUID()
+				+ '"' 
+				+ ")";
+		ResultSet rs = stmt.executeQuery(sql);
+		return Common.Success;
+	}
+	
+	public static String Withdraw(Transaction t, Connection con) throws SQLException {
+		Statement stmt = con.createStatement();
+		String sql = "INSERT INTO `trans` "
+				+ "(`TransID`,`TransName`,`SenderID`,`ReceiverID`,`Money`,`Datetime`) "
+				+ "VALUES (" 
+				+ '"' 
+				+ t.getTransTime() 
+				+ '"' 
+				+ ", " 
+				+ '"'
+				+ t.gettransname()  
+				+ '"'
+				+ ", " 
+				+ '"' 
+				+ t.getsenderUUID() 
+				+ '"' 
+				+ ", " 
+				+ '"' 
+				+ t.getreceiverUUID() 
+				+ '"' 
+				+ ", "  
+				+ t.getmoney() 
+				+ ", " 
+				+ '"' 
+				+ t.gettransUUID()
+				+ '"' 
+				+ ")";
+		ResultSet rs = stmt.executeQuery(sql);
+		return Common.Success;
+	}
+	
 }
