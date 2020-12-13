d954759295a9275e102a0ea97eeca0c2e34542fc
“jnzbfgjd” <m15927337875@163.com>
2020-12-13 17:44:29 +0800

    trans

--- src/CentralSystem/Transaction.java
+++ src/CentralSystem/Transaction.java
@@ -1,5 +1,8 @@
 package CentralSystem;
+import java.sql.Date;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.UUID;
 
 public class Transaction {
@@ -9,13 +12,26 @@ public class Transaction {
 	private String receiver;
 	private UUID senderUUID;
 	private UUID receiverUUID;
+	private double money;
 	
-	public Transaction(LocalDateTime transferTime, String sender, String receiver, UUID senderUUID, UUID receiverUUID) {
+	public Transaction(LocalDateTime transferTime, String sender, String receiver, UUID senderUUID, UUID receiverUUID,double money) {
 		this.transferTime = transferTime;
 		this.sender = sender;
 		this.receiver = receiver;
 		this.senderUUID = senderUUID;
 		this.receiverUUID = receiverUUID;
+		this.money=money;
+	}
+	
+	public Transaction(Date transferTime,String sender,String receiver,String senderUUID,String receiverUUID,double money) {
+		Instant instant=transferTime.toInstant();
+		ZoneId zoneId = ZoneId.systemDefault();
+		this.transferTime=instant.atZone(zoneId).toLocalDateTime();
+		this.sender=sender;
+		this.receiver=receiver;
+		this.receiverUUID=UUID.fromString(receiverUUID);
+		this.senderUUID=UUID.fromString(senderUUID);
+		this.money=money;
 	}
 	
 }
